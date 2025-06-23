7eb3b5dd76a2be29ed1c3744d34298721906ed6b
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-23 19:50:12 -0300

    Add model index file

---
+++ backend/src/models/index.js
@@ -0,0 +1,11 @@
+const User = require('./User');
+const Equipment = require('./Equipment');
+const InsurancePolicy = require('./InsurancePolicy');
+const Band = require('./Band');
+
+module.exports = {
+  User,
+  Equipment,
+  InsurancePolicy,
+  Band
+};
